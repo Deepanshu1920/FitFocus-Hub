8f3102972a48f83ff056b4808a44ac498fec2613
deepanshu1920 <deepanshu1920>
2024-05-02 12:03:35 +0530

    joint js but not working

---
+++ Myapp/static/myapp/js/connect_to_home_page.js
@@ -0,0 +1,11 @@
+
+function greet(name) {
+    console.log('Hello, ' + name + '!');
+  }
+  
+  const message = 'Welcome to my website';
+  
+  // And you can call functions and use variables in your code.
+  
+  greet('Deepanshu');
+  console.log(message);
